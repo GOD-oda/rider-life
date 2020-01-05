dfc016c0edefe08c0ea60ea7a4a7f09a7ef57078
t.oda <takahiro.tech.oda@gmail.com>
2020-01-05 22:41:37 +0900

    Script to save stations

---
+++ src/firebase/insert_stations.js
@@ -0,0 +1,22 @@
+const admin = require('firebase-admin')
+const serviceAccount = require('./data/serviceAccountKey.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://rider-life.firebaseio.com"
+});
+
+const stationsRef = admin.firestore().collection('stations');
+
+const stations = require('./data/stations.json')
+for (let i = 0; i < stations.length; i++) {
+  let d = new Date()
+  let time = d.getTime();
+  let params = {
+    name: stations[i]['name'],
+    createdAt: time,
+    updatedAt: time
+  }
+
+  stationsRef.doc('station_' + stations[i]['id']).set(params)
+}
