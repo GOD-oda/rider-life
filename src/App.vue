0f5faeebb5563420dcec71dee5cc9b3590ce98e4
t.oda <takahiro.tech.oda@gmail.com>
2020-01-29 00:16:04 +0900

    Use user data

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
   <div id="app">
-    <router-view/>
+    <Header/>
+
+    <div id="body">
+      <router-view/>
+    </div>
   </div>
 </template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -11,6 +25,10 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
+  #body {
+    background-color: #f6f8fa;
+  }
 }
 
 #nav {
---
+++ src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+<!--  <div id="header">-->
+<!--    <div id="logo">-->
+<!--      <img src="https://placehold.jp/50x50.png" alt="">-->
+<!--    </div>-->
+
+<!--    <div id="title">-->
+<!--      Rider-life-->
+<!--    </div>-->
+
+<!--    <div id="user-image">-->
+<!--      <img src="https://placehold.jp/50x50.png" alt="">-->
+<!--    </div>-->
+<!--  </div>-->
+  <div id="header">
+    <div id="logo">
+      <img src="https://placehold.jp/50x50.png" alt="">
+    </div>
+    <div id="title">
+      Rider-life
+    </div>
+    <div id="user-image">
+      <img src="https://placehold.jp/50x50.png" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="scss">
+#header {
+  display: flex;
+  justify-content: space-between;
+  height: 56px;
+  align-items: center;
+
+  #logo, #user-image {
+    img {
+      vertical-align: middle;
+    }
+  }
+}
+</style>
